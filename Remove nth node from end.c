d4d6d5722c58b5d3dd727bf1b04b38a4a703c48b
Prikshit2803 <119496129+Prikshit2803@users.noreply.github.com>
2022-12-01 01:05:30 +0530

    Create Remove nth node from end.c

---
+++ Remove nth node from end.c
@@ -0,0 +1,99 @@
+class Solution {
+
+    ListNode* reverse(ListNode * head){
+        if(head==NULL)
+        return NULL;
+        
+       ListNode * cur=head;
+        ListNode* prev=NULL;
+        while(cur!=NULL){
+            ListNode * temp=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=temp;
+            
+            
+        }
+        return prev;
+    }
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+      
+      need to work on this approach
+        /*int cnt=0;
+
+        if(head->next==NULL)
+        return NULL;
+
+        ListNode* cur=reverse(head);
+        //return cur;
+        ListNode* prev=NULL;
+
+        /*if(head->next==NULL)
+        return NULL;
+
+        while(cnt!=n-1 && cur){
+            cnt++;
+            prev=cur;
+            cur=cur->next;
+        }
+        if(cur!=NULL && cur->next!=NULL)
+        prev->next=cur->next;
+
+        if(cur->next==NULL)
+        prev->next=NULL;
+        
+         head=reverse(cur);
+        return head;*/
+
+         The following approach works 
+        /*int cnt=0;
+         ListNode* cur=head;
+
+         if(n==1 && head->next==NULL||head==NULL) return NULL;
+        while(cur && cur->next){
+            cur=cur->next;
+            cnt++;
+        }
+        int k=cnt-n;
+        if(k<0){
+            return head->next;
+        }
+        ListNode* c=head;
+       /* while(k>=0){
+           // p=c;
+            c=c->next;
+            k--;
+        }
+        for(int i=0;i<k;i++){
+            c=c->next;
+            
+        }
+        c->next=c->next->next;
+        return head;*/
+           
+// approach works
+           ListNode * start= new ListNode();
+           start->next=head;
+         ListNode* f=start;
+          ListNode* s=start;
+
+          int k=n;
+          for(int i=0;i<k;i++){
+            f=f->next;
+            
+        }
+
+        while(f->next!=NULL)
+        {
+             f=f->next;
+            s=s->next;
+           
+        }
+        
+        s->next=s->next->next;
+
+        return start->next;
+
+    }
+};
